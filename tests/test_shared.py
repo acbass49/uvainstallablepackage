12d83a64cc81197ad3a4d11fd62b52193bc3fa0d
acbass49 <acbass49@gmail.com>
2023-04-01 16:36:49 -0400

    updated file and tests

--- tests/test_shared.py
+++ tests/test_shared.py
@@ -1,8 +1,25 @@
 import sys
 sys.path.append('.')
 import shared as sh
+import pytest
 
 def test_clean_string():
     test_str = " This! is      a ,test string  "
 
     assert "This is a test string" == sh.clean_string(test_str), "String <{}> not cleaned as expected".format(test_str)
+
+def test_another_test():
+    assert 1 == 1
+
+@pytest.mark.xfail
+def test_failed_test1():
+    assert 1 == 2
+
+@pytest.mark.skip(reason="bug we want to fix")
+def test_failed_test2():
+    assert 1 == 2
+
+@pytest.mark.skipif(sys.platform == 'darwin', reason="requires windows system to test")
+def test_failed_test3():
+    print("My platform is", sys.platform)
+    assert 1 == 2
